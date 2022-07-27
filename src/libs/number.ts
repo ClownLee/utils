c6a7b16cc73c52edfb9b6ef6c0cf368555b59704
ele_11_ven <lwentao@hotmail.com>
2022-07-27 03:56:17 +0000

    format

--- src/libs/number.ts
+++ src/libs/number.ts
@@ -1,11 +1,21 @@
+/**
+ * 判断是否是数字
+ * @param {number | string} num 数字
+ * @returns {boolean} 布尔值
+ */
 export function isNumber(num: string | number): boolean {
   return new RegExp(/^-?[0-9]+(.[0-9]+)?$/).test(`${num}`);
 }
 
+/**
+ * 数字转千分位
+ * @param {number | string} num 数字
+ * @returns {string} 千分位数字
+ */
 export function thousandth(num: string | number): string {
   num = `${num}`; // To String
 
-  if (!new RegExp(/^-?[0-9]+(.[0-9]+)?$/).test(num)) {
+  if (!isNumber(num)) {
     throw new Error('输入参数必须是数字或数字型字符串');
   }
 
