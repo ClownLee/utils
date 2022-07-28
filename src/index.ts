bdd360403589fad6bd53c94f38b6fcf5ea2543f2
unknown <lwentao@hotmail.com>
2022-07-29 01:24:37 +0800

    delete console

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,5 @@ import { isNumber, thousandth, zeroFilling } from './components/number';
 import { encode, decode } from './components/html';
 import { arrayToTree, treeToArray, findParentTree, findParentArr, deepClone  } from './components/arrs';
 
-const html = '<div><h1>哈哈哈哈</h1><h2>嘿嘿嘿嘿</h2></div>'
-const text = encode(html)
-console.log(text);
 
 export { isNumber, thousandth, zeroFilling, debounce, throttle, encode, decode, arrayToTree, treeToArray, findParentTree, findParentArr, deepClone };
